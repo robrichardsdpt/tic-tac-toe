ac59a4ec655f2facdc2d699d7b832c4610431ccc
robrichardsdpt <robrichardsdpt@gmail.com>
2020-09-01 12:40:36 -0400

    Updated popper
    Incldued popper.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
-
+import 'popper.js'
 import 'bootstrap'
 
 // allows usage of new JS features
